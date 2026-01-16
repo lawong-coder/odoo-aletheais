331c19315b5c8635e1af737c0205ba8d167aa942
Lawrence <lawongcy@gmail.com>
2026-01-16 20:10:08 +0800

    Add license keys to custom modules and include server-tools in addons path

--- custom-addons/sale_order_label_override/__manifest__.py
+++ custom-addons/sale_order_label_override/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Sale Order Label Override",
     "version": "18.0.1.0.0",
+    "license": "LGPL-3",
     "depends": ["sale", "account", "mail"],
     "data": [
         "views/sale_order_report.xml",
--- custom-addons/website_footer_override/__manifest__.py
+++ custom-addons/website_footer_override/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name": "Website Footer Branding Override",
     "version": "18.0.1.0.0",
+    "license": "LGPL-3",
     "depends": ["website"],
     "data": [
         "views/footer_override.xml",
